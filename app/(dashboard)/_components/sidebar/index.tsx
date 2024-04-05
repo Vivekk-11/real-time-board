1019b63052556145e28fd168a40caccecdbaee21
vivek <chimnanivivek14@gmail.com>
2024-04-05 09:56:57 +0530

    layout for dashboard section

---
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -0,0 +1,7 @@
+export const Sidebar = () => {
+  return (
+    <aside className="fixed left-0 z-[1] bg-blue-950 h-full text-white flex flex-col gap-y-4 w-[60px]">
+      Sidebar
+    </aside>
+  );
+};
